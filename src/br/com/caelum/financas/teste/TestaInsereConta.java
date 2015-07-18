6d2c474a99d4e8e04569508c3cc9e5c92609eef0
Bruno Souza <brunoprogramadorjava>
2015-07-18 11:52:51 -0300

    Comparando a diferenca com um metodo static para retornar EntityManagerFactory

--- src/br/com/caelum/financas/teste/TestaInsereConta.java
+++ src/br/com/caelum/financas/teste/TestaInsereConta.java
@@ -5,6 +5,7 @@ import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
 
 import br.com.caelum.financas.modelo.Conta;
+import br.com.caelum.financas.util.JPAUtil;
 
 public class TestaInsereConta {
 
@@ -32,6 +33,28 @@ public class TestaInsereConta {
 
 		long fim = System.currentTimeMillis();
 		System.out.println("Executando em: " + (fim - inicio) + "ms");
+		
+		
+		
+		long inicio2 = System.currentTimeMillis();
+
+		EntityManager manager2 = new JPAUtil().getEntityManager();
+
+		Conta conta2 = new Conta();
+		conta2.setTitular("Dayani Souza");
+		conta2.setBanco("Banco do Brasil");
+		conta2.setNumero("123456-6");
+		conta2.setAgencia("0999");
+
+		manager2.getTransaction().begin();
+		manager2.persist(conta2);
+		manager2.getTransaction().commit();
+		manager2.close();
+
+		System.out.println("Conta gravada com sucesso!");
+
+		long fim2 = System.currentTimeMillis();
+		System.out.println("Executando em: " + (fim2 - inicio2) + "ms");
 	}
 
 }
